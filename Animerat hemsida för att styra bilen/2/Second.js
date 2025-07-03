a987f945b2c6c3ff340c2346d0a61e55830d2632
Mohammad Hajjo <102748292+HamadaHajjo@users.noreply.github.com>
2025-07-03 22:02:59 +0200

    Add files via upload

---
+++ Animerat hemsida för att styra bilen/2/Second.js
@@ -0,0 +1,4 @@
+document.getElementById('start-button').addEventListener('click', function(){
+    // Redirect to third.html when start button is clicked
+    window.location.href = '/3/Third.html';
+});
